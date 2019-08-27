19e650a4d36f5c1455649f3d34e462a5943512fd
Mikael MARESCAUX <Mikael.marescaux@eden-innovations.com>
2019-08-27 13:54:17 +0200

    start http://melonjs.github.io/tutorial-platformer

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -7,14 +7,37 @@ game.PlayerEntity = me.Entity.extend({
      * constructor
      */
     init:function (x, y, settings) {
-        // call the constructor
-        this._super(me.Entity, 'init', [x, y , settings]);
+      // call the constructor
+      this._super(me.Entity, 'init', [x, y , settings]);
+
+      // max walking & jumping speed
+      this.body.setMaxVelocity(3, 15);
+      this.body.setFriction(0.4, 0);
+
+      // set the display to follow our position on both axis
+      me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH, 0.4);
+      
+      // ensure the player is updated even when outside of the viewport
+      this.alwaysUpdate = true;
+
+      // define a basic walking animation (using all frames)
+      this.renderable.addAnimation("walk",  [0, 1, 2, 3, 4, 5, 6, 7]);
+
+      // define a standing animation (using the first frame)
+      this.renderable.addAnimation("stand",  [0]);
+
+      // set the standing animation as default
+      this.renderable.setCurrentAnimation("stand");      
     },
 
     /**
      * update the entity
      */
     update : function (dt) {
+
+      this.body.force.x = 0;
+      // change to the standing animation
+      this.renderable.setCurrentAnimation("stand");
 
         // apply physics to the body (this moves the entity)
         this.body.update(dt);
